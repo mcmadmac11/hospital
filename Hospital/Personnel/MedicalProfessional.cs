4998da76ad8c237c08d7c4486461c95f9097f8ad
mcmadmac11 <mcmanus.dbm@gmail.com>
2015-11-30 17:27:26 -0600

    re up

--- Hospital/Personnel/MedicalProfessional.cs
+++ Hospital/Personnel/MedicalProfessional.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Personnel
 {
-<<<<<<< HEAD
+
     class MedicalProfessional : Person
-=======
+        
     public class MedicalProfessional
->>>>>>> 79f5967df807567d7bc4f6798fa51d1bfce351c3
+
     {
 
 
